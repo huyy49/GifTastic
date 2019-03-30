c3837112720569905db560da494c9ae1b84c9ded
Yangyin Hu <yangyinhu@Toms-MacBook-Pro.local>
2019-03-30 14:50:52 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GifTastic
+## Overview
+
+Create a webpage that allows the user to search for GIFs. Although the topic chosen is Family Guy figures, the user can actually search for anything of interest. When the user enters a search word and clicks on the Submit button, 10 GIFs will display on the screen, with the details on rating and title. There is also a Download button for each GIF. The search term will be stored at the top of the screen. If the user submits another word, another 10 GIFs will show on top of the previous 10 GIFs. The user can clear the page by clicking on the Clear button.
+
+## Link to Homework
+https://huyy49.github.io/GifTastic/
---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  var key = "vyvSZGsFRL9ZbUxfKfz5utTKA68j50cj";
+  var queryURL = "";
+  var topics = [];
+
+  var renderGif = function (term) {
+    queryURL = "https://api.giphy.com/v1/gifs/search?q="+ term + "&api_key=" + key + "&limit=10";
+    console.log(queryURL);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+      for(var i=0; i<10; i++){
+        var still = response.data[i].images.original_still.url;
+        var animate = response.data[i].images.original.url;
+        var rating = response.data[i].rating;
+        var title = response.data[i].title;
+        $("#display-images").prepend($("<div>").addClass("card float-left my-2 mr-2 border-0").attr("id", "card-container"));
+        $("#card-container").append($("<h5>").addClass("card-title").html("Rating: " + rating.toUpperCase()));
+        $("#card-container").append($("<p6>").addClass("card-text mw-40").html("Title: " + title));
+        $("#card-container").append($("<div>").addClass("card-img-bottom").html("<img src='" + still + "' data-still='" + still + "' data-animate='" + animate + "' data-state='still' class='gif img rounded' height='200'></img>"));
+        $("#card-container").append($("<div>").addClass("card-text my-1").html("<a href =" + animate + " download><button>Download</button></a>"));
+      }
+    });
+  }
+
+  $("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    console.log($("#search-term").val());
+
+    if(!$("#search-term").val()) {
+      alert('Entry is empty!');
+    } else {
+    renderGif($("#search-term").val());
+
+    $("#display-topics").append($("<div>").addClass("float-left my-2 mr-2").html("<button type='button' class='btn btn-primary topics-button' id='topics-" + topics.length + "'>" + $("#search-term").val() + "</button>"));
+
+    topics.push($("#search-term").val());
+    console.log(topics);
+    }
+  });
+
+  $(document).on("click", ".gif", function(event) {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else if (state === "animate") {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+  $(document).on("click", ".topics-button", function(event) {
+    var topic = $(this).attr("id");
+    console.log(topic);
+    var topicSelect = topic.match(/\d+/g).map(Number)[0];
+    console.log(topicSelect);
+    renderGif(topics[topicSelect]);
+  });
+
+  $("#reload").click(function() {
+    location.reload();
+});
+});
